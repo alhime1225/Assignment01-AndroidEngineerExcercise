6dcdffc184c464c4402d6029f1e2a48fc8a3b879
aisha0926 <aisha.0926@hotmail.com>
2021-01-22 19:14:50 +0000

    Adding all files

--- app/src/main/java/com/example/androidengineerexercise/FirstScreen.java
+++ app/src/main/java/com/example/androidengineerexercise/FirstScreen.java
@@ -2,7 +2,9 @@ package com.example.androidengineerexercise;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class FirstScreen extends AppCompatActivity {
 
@@ -10,5 +12,10 @@ public class FirstScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first_screen);
+    }
+
+    public void createAccount(View view){
+        Intent intent = new Intent(this, NextScreen.class);
+        startActivity(intent);
     }
 }
